4e0ab7bb1599a0e49c2bb1267bb491f0cf67c478
Domenic Schoenfisch <domenicschoenfischqgmail.com>
2025-10-21 00:00:02 +0200

    Klasse Domenic neu

--- Domenic/java/Main.java
+++ Domenic/java/Main.java
@@ -3,6 +3,6 @@ package Main.java;
 
 public class Domenic {
     public static void main(String[] args) {
-        System.out.println("Hallo, ich bin Domenic");
+        System.out.println("Hallo Welt");
     }
 }
